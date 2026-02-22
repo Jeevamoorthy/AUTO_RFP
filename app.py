312ab9e540bd6994e03e1dcddcb8c4d7efc22941
Jeeva M I <jeevamissvmins34@gmail.com>
2026-02-22 09:51:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -92,7 +92,7 @@ with st.sidebar:
     if "Gemini" in provider:
         model_version = st.selectbox(
             "Gemini Model",
-            ["gemini-2.5-pro", "gemini-2.5-flash", "gemini-2.0-flash"]
+            [ "gemini-2.5-flash", "gemini-2.0-flash","gemini-2.5-pro"]
         )
 
     elif provider == "OpenAI":
